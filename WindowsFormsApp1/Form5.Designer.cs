6dfff65246e4c5deb7321588660fd7ed8773f3c9
samue <sninasunta@uteq.edu.ec>
2026-01-18 01:40:33 -0500

    Arreglo de boton ir produ

--- WindowsFormsApp1/Form5.Designer.cs
+++ WindowsFormsApp1/Form5.Designer.cs
@@ -77,22 +77,23 @@
             this.btnIrProduccion.UseVisualStyleBackColor = false;
             this.btnIrProduccion.Click += new System.EventHandler(this.btnIrProduccion_Click);
             // 
-            // button1
+            // btnIrProdu
             // 
-            this.button1.Location = new System.Drawing.Point(299, 338);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(40, 38);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnIrProdu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnIrProdu.Location = new System.Drawing.Point(290, 333);
+            this.btnIrProdu.Name = "btnIrProdu";
+            this.btnIrProdu.Size = new System.Drawing.Size(49, 45);
+            this.btnIrProdu.TabIndex = 2;
+            this.btnIrProdu.Text = "->";
+            this.btnIrProdu.UseVisualStyleBackColor = false;
+            this.btnIrProdu.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(921, 489);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnIrProdu);
             this.Controls.Add(this.btnIrCrianza);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form5";
@@ -106,6 +107,6 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnIrCrianza;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnIrProdu;
     }
 }
